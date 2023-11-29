b1e34c7486ba4a7f216fb39d6dfb88b8bba0d96b
Mustafa Saifee <mustafa.saifee@undp.org>
2023-11-29 19:01:27 +0200

    Add sections in data list

--- src/GrapherComponent/DataList/DataListForCountry.tsx
+++ src/GrapherComponent/DataList/DataListForCountry.tsx
@@ -2,8 +2,9 @@
 import { useState } from 'react';
 import sortBy from 'lodash.sortby';
 import { Input } from 'antd';
-import { format } from 'd3-format';
+import uniqBy from 'lodash.uniqby';
 import { DataType, IndicatorMetaDataType } from '../../Types';
+import { customFormat } from '../../Utils/CustomFormat';
 
 interface Props {
   data: DataType[];
@@ -22,70 +23,7 @@ export function DataListForCountry(props: Props) {
   const dataFilteredByCountry = data.filter(
     d => d['Country or Area'] === country,
   )[0].data;
-
-  function customFormat(num: number, dataKey: string): string {
-    const specificKeys = [
-      'averted_deaths',
-      'tobacco_attributable_deaths',
-      'annual_deaths_averted',
-      'NCD_deaths_per_1000',
-      '15y_deaths_averted_total',
-      '15y_hly_total',
-      '15y_hly_tobacco',
-      '15y_deaths_averted_total',
-      '15y_strokes_averted_total',
-      '15y_IHD_averted_total',
-    ];
-
-    const roiKeys = [
-      '15y_ROI_alcohol',
-      '15y_ROI_salt',
-      '15y_ROI_physicalActivity',
-      '15y_ROI_clinical',
-      '15y_ROI_tobacco',
-      'all_ROI_15years',
-    ];
-    const yearIndicator = ['ncd_reference_year', 'tobacco_reference_year'];
-
-    const formatNum = (
-      value: number,
-      suffix: string,
-      forceOneDecimal = false,
-    ) => {
-      const formatSpecifier = forceOneDecimal
-        ? '.2f'
-        : value <= 20
-        ? '.1f'
-        : '.0f';
-      let formattedValue = format(formatSpecifier)(value);
-      // Remove trailing zeros and unnecessary decimal points
-      formattedValue = formattedValue.replace(/(\.\d*[1-9])0+$|\.0+$/, '$1');
-      return `${formattedValue}${suffix}`;
-    };
-
-    if (yearIndicator.includes(dataKey)) {
-      return format('.0f')(num); // Format as whole number without comma
-    }
-
-    if (specificKeys.includes(dataKey)) {
-      return format(',')(Math.round(num));
-    }
-
-    const isROIMetric = roiKeys.includes(dataKey);
-    const shouldForceOneDecimal = isROIMetric && num < 1;
-
-    if (num >= 1000000000) {
-      return formatNum(num / 1000000000, ' Bil', shouldForceOneDecimal);
-    }
-    if (num >= 1000000) {
-      return formatNum(num / 1000000, ' Mil', shouldForceOneDecimal);
-    }
-    if (num >= 1000) {
-      return formatNum(num / 1000, ' K', shouldForceOneDecimal);
-    }
-    return formatNum(num, '', shouldForceOneDecimal);
-  }
-
+  const focusAreas = uniqBy(indicators, d => d.FocusArea).map(d => d.FocusArea);
   return (
     <div style={{ width: '100%' }}>
       <div
@@ -124,58 +62,76 @@ export function DataListForCountry(props: Props) {
             Value
           </div>
         </div>
-        {sortBy(filteredIndicatorsBySearch, d => d.Indicator).map((d, i) =>
-          dataFilteredByCountry.findIndex(el => el.indicator === d.DataKey) !==
-          -1 ? (
+        {focusAreas.map((f, j) => (
+          <div key={j}>
             <div
-              key={i}
-              className='undp-table-row padding-top-05'
-              style={{ backgroundColor: 'var(--white)' }}
+              style={{
+                backgroundColor: 'var(--gray-600)',
+                color: 'var(--white)',
+                width: '100%',
+                padding: 'var(--spacing-05)',
+              }}
             >
-              <div
-                style={{
-                  width: '70%',
-                  fontSize: '1.1rem',
-                  paddingLeft: '1rem',
-                }}
-                className='undp-table-row-cell'
-              >
-                <div className='undp-typography'>{d.Indicator}</div>
-              </div>
-              <div
-                style={{ width: '30%', paddingRight: '1rem' }}
-                className='undp-table-row-cell align-right'
-              >
-                {dataFilteredByCountry.findIndex(
-                  el => el.indicator === d.DataKey,
-                ) === -1 ? (
-                  'NA'
-                ) : (
-                  <h5 className='undp-typography margin-bottom-00 bold'>
-                    {d.LabelPrefix ? `${d.LabelPrefix} ` : ''}
+              <h6 className='undp-typography margin-bottom-00'>{f}</h6>
+            </div>
+            {sortBy(
+              filteredIndicatorsBySearch,
+              d => d.Indicator && d.FocusArea === f,
+            ).map((d, i) =>
+              dataFilteredByCountry.findIndex(
+                el => el.indicator === d.DataKey,
+              ) !== -1 ? (
+                <div
+                  key={i}
+                  className='undp-table-row padding-top-05'
+                  style={{ backgroundColor: 'var(--white)' }}
+                >
+                  <div
+                    style={{
+                      width: '70%',
+                      fontSize: '1.1rem',
+                      paddingLeft: '1rem',
+                    }}
+                    className='undp-table-row-cell'
+                  >
+                    <div className='undp-typography'>{d.Indicator}</div>
+                  </div>
+                  <div
+                    style={{ width: '30%', paddingRight: '1rem' }}
+                    className='undp-table-row-cell align-right'
+                  >
                     {dataFilteredByCountry.findIndex(
                       el => el.indicator === d.DataKey,
-                    ) !== -1
-                      ? customFormat(
-                          dataFilteredByCountry[
-                            dataFilteredByCountry.findIndex(
-                              el => el.indicator === d.DataKey,
+                    ) === -1 ? (
+                      'NA'
+                    ) : (
+                      <h5 className='undp-typography margin-bottom-00 bold'>
+                        {d.LabelPrefix ? `${d.LabelPrefix} ` : ''}
+                        {dataFilteredByCountry.findIndex(
+                          el => el.indicator === d.DataKey,
+                        ) !== -1
+                          ? customFormat(
+                              dataFilteredByCountry[
+                                dataFilteredByCountry.findIndex(
+                                  el => el.indicator === d.DataKey,
+                                )
+                              ].value,
+                              d.DataKey,
                             )
-                          ].value,
-                          d.DataKey,
-                        )
-                      : dataFilteredByCountry[
-                          dataFilteredByCountry.findIndex(
-                            el => el.indicator === d.DataKey,
-                          )
-                        ].value}
-                    {d.LabelSuffix ? ` ${d.LabelSuffix}` : ''}
-                  </h5>
-                )}
-              </div>
-            </div>
-          ) : null,
-        )}
+                          : dataFilteredByCountry[
+                              dataFilteredByCountry.findIndex(
+                                el => el.indicator === d.DataKey,
+                              )
+                            ].value}
+                        {d.LabelSuffix ? ` ${d.LabelSuffix}` : ''}
+                      </h5>
+                    )}
+                  </div>
+                </div>
+              ) : null,
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
